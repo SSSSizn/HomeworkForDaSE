c7ac1f8b2adf7b163e5dfe32115965c579367575
SIZN <136679957+SSSSizn@users.noreply.github.com>
2024-04-20 21:03:06 +0800

    Add files via upload

---
+++ OS/testTicket.c
@@ -0,0 +1,18 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[])
+{
+
+    int number = atoi(argv[1]);
+    settickets(number);
+
+    while (1)
+    {
+        /* code */
+        /* just for delay */
+    }
+
+    exit();
+}
